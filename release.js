0091c030d6542d10370869a71f6a9d2e3cc2ef87
榊原昌彦 <sakakibara@rdlabo.jp>
2023-12-12 14:58:05 +0900

    feat(): prepare auto release

---
+++ release.js
@@ -0,0 +1,19 @@
+import { updatePackage } from 'write-pkg';
+import path from 'path';
+import pkg from './package.json' assert { type: 'json' };
+import { exec } from 'child_process';
+
+const workspaces = ['photo-editor'];
+
+workspaces.forEach(async (workspace) => {
+  const buildPackagePath = path.resolve('./projects/' + workspace);
+  const releasePackagePath = path.resolve('./dist/' + workspace);
+  await updatePackage(releasePackagePath + '/package.json', { version: pkg.version });
+  exec(`ng build ${buildPackagePath} && npm publish --prefix ${releasePackagePath}`, (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(stdout);
+  });
+});
