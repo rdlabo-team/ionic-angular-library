df008d62d8af884ef88d7f9a0ed8b5d58b5f4ea0
榊原昌彦 <sakakibara@rdlabo.jp>
2023-12-12 14:59:54 +0900

    chore

--- release.js
+++ release.js
@@ -9,7 +9,7 @@ workspaces.forEach(async (workspace) => {
   const buildPackagePath = path.resolve('./projects/' + workspace);
   const releasePackagePath = path.resolve('./dist/' + workspace);
   await updatePackage(releasePackagePath + '/package.json', { version: pkg.version });
-  exec(`ng build ${buildPackagePath} && npm publish --prefix ${releasePackagePath}`, (err, stdout, stderr) => {
+  exec(`ng build ${buildPackagePath} --configuration=production && npm publish --prefix ${releasePackagePath}`, (err, stdout, stderr) => {
     if (err) {
       console.error(err);
       return;
