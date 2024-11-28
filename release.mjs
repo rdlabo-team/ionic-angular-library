d37a1627b0e0a884c3d9c534adb0b87a8037e66b
榊原昌彦 <sakakibara@rdlabo.jp>
2024-11-28 16:21:22 +0900

    chore

--- release.mjs
+++ release.mjs
@@ -1,6 +1,6 @@
 import { updatePackage } from 'write-pkg';
 import path from 'path';
-import pkg from './package.json' assert { type: 'json' };
+import pkg from './package.json' with { type: 'json' };
 import { exec } from 'child_process';
 
 const workspaces = ['photo-editor', 'scroll-header', 'scroll-strategies'];
